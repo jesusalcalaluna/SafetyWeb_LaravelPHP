989722f42be2d43448f2bf88c3a0a403735c6e34
alcala <jesusalcalaluna@gmail.com>
2021-02-13 02:14:09 -0600

    Update css.blade.php

--- resources/views/globals/css.blade.php
+++ resources/views/globals/css.blade.php
@@ -1,15 +1,15 @@
 <!-- Bootstrap core CSS -->
-<link href="css/css page/bootstrap.min.css" rel="stylesheet">
+<link href="{{ asset('css/css page/bootstrap.min.css')}}" rel="stylesheet">
 
 <!-- Font Awesome CSS -->
-<link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{ asset('fonts/font-awesome/css/font-awesome.min.css')}}">
 
 <!-- Flex Slider -->
-<link rel="stylesheet" href="css/css page/flexslider.css">
+<link rel="stylesheet" href="{{ asset('css/css page/flexslider.css')}}">
 
 <!-- Owl Carousel -->
-<link rel="stylesheet" href="css/css page/owl.carousel.min.css">
-<link rel="stylesheet" href="css/css page/owl.theme.min.css">
+<link rel="stylesheet" href="{{ asset('css/css page/owl.carousel.min.css')}}">
+<link rel="stylesheet" href="{{ asset('css/css page/owl.theme.min.css')}}">
 
 <!-- Custom styles for this template -->
-<link href="css/css page/style.css" rel="stylesheet">
+<link href="{{ asset('css/css page/style.css')}}" rel="stylesheet">
