eae122e6a4cb823236109f65b4b5168b4f92add8
ProgramadorSMT5 <82223163+ProgramadorSMT5@users.noreply.github.com>
2021-04-13 08:42:12 -0500

    Update contact-info.blade.php

--- resources/views/globals/contact-info.blade.php
+++ resources/views/globals/contact-info.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="col-md-3 logo center-block float-unset w-15">
             <!-- Main Logo -->
-            <a href="index.html"><img src="images/logo CMT.png" alt="Offshore Logo" /></a>
+            <a href="{{ route("index")}}"><img src="images/logo CMT.png" alt="Offshore Logo" /></a>
             <!-- Responsive Toggle Menu -->
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
                 <span class="sr-only"> Menu </span>
