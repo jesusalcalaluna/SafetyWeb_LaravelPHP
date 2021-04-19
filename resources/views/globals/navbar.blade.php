d85dfa53e485398545050dca7ebfa78abbb3a209
ProgramadorSMT5 <82223163+ProgramadorSMT5@users.noreply.github.com>
2021-04-19 08:53:15 -0500

    Update navbar.blade.php

--- resources/views/globals/navbar.blade.php
+++ resources/views/globals/navbar.blade.php
@@ -2,6 +2,7 @@
     <div class="container">
         <ul class="main-menu">
             <li class="@if(Route::current()->getName() == 'index') active @endif"> <a href="{{route("index")}}"> Home </a></li>
+            <li class="@if(Route::current()->getName() == 'dashboard') active @endif"> <a href="{{route('dashboard')}}"> Panel de Administrador </a></li>
             <li class="dropdown"> <a href="#" data-toggle="dropdown"> Juntas
                 <i class="fa fa-chevron-down dropdown-toggle"> </i>  </a>
                 <ul>
